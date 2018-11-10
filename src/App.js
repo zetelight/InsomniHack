1d3dbfa7d6ad7a8315b3b7f6b60c14b469e48501
zedzedQ <zhiqianz@uoregon.edu>
2018-11-10 11:48:11 -0800

    init

--- src/App.js
+++ src/App.js
@@ -21,13 +21,59 @@ class App extends Component {
         };
     }
 
+    unpickTraverse() {
+      var unpicked = this.state.columns['columnUnpicked'].taskIds;
+      var i;
+      var j
+      for (i=0;i<unpicked.length;i++) {
+        const prerequest = this.state.cisCourses[unpicked[i]].preReq
+        var flag = true
+        for (j=0;j<prerequest.length;j++) {
+          if (unpicked.includes(prerequest[j])) {
+            flag = false
+          }
+        }
+        this.state.cisCourses[unpicked[i]].isMoveble = flag
+          
+      }
+    }
+
+    planTraverse(){
+      // dictionary of tasks
+      // key is the class name (string)
+      // values is the class instance
+      var newArray = [];
+      var curCol = this.state.columns['columnTaken'];
+      for (var j=0; j<curCol.taskIds.length; j++){
+        var curCourse = this.state.cisCourses[curCol.taskIds[j]];
+        newArray.push({key:curCol.taskIds[j], value: curCourse})
+      }
+      for(var i=1; i<=Object.keys(this.state.columns).length-2; ++i){
+          curCol = this.state.columns['columnTerm' + i];
+          for (j=0; j<curCol.taskIds.length; j++){
+              curCourse = this.state.cisCourses[curCol.taskIds[j]];
+              newArray.push({key:curCol.taskIds[j], value: curCourse})
+          }
+      }
+      console.log(newArray);
+      for (var myKey in newArray){
+        var course = newArray[myKey];
+          for (var myIn in course.value.preReq){
+            var pre = course.value.preReq[myIn]
+            this.state.cisCourses[pre].isMoveble = false;
+          }
+      }
+      console.log(this.state.cisCourses);
+      
+  }
+
     onDragEnd = result => {
         const { destination, source, draggableId } = result;
 
         if (!destination) {
             return;
         }
-
+        
         if (
             destination.droppableId === source.droppableId &&
             destination.index === source.index
@@ -61,6 +107,10 @@ class App extends Component {
             this.setState(newState);
             return;
         }
+
+        this.planTraverse();
+        this.unpickTraverse();
+        console.log(this.state.cisCourses);
 
         const startTaskIds = Array.from(start.taskIds);
         startTaskIds.splice(source.index, 1);
@@ -108,7 +158,7 @@ class App extends Component {
 
         }
 
-        for (var i in this.state.columnOrder){
+        for (i in this.state.columnOrder){
             l.push(this.state.columnOrder[i])
         }
 
